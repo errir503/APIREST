7f9f83bbcc7f7219fa1a8824a1b93fa2d33085a4
Junior Cunha <manoelricardo38@gmail.com>
2020-11-13 15:39:33 -0300

    ajeitando o arquivo do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,26 @@
-// module.exports={
-//   dialect: "postgres",
-//   port: 5432,
-//   host: process.env.HOST ,
-//   schema: "public",
-//   database: process.env.DATABASE ,
-//   username:process.env.USER ,
-//   password: process.env.PASSWORD ,
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//   },
-// }
 module.exports = {
-    dialect: "postgres",
-    port: 5432,
-    host: "localhost",
-    schema: "public",
-    database: "smart",
-    username: "postgres",
-    password: "1234",
-    define: {
-        timestamps: true,
-        underscored: true,
-    },
-}
+        dialect: "postgres",
+        port: 5432,
+        host: process.env.HOST,
+        schema: "public",
+        database: process.env.DATABASE,
+        username: process.env.USER,
+        password: process.env.PASSWORD,
+        define: {
+            timestamps: true,
+            underscored: true,
+        },
+    }
+    // module.exports = {
+    //     dialect: "postgres",
+    //     port: 5432,
+    //     host: "localhost",
+    //     schema: "public",
+    //     database: "smart",
+    //     username: "postgres",
+    //     password: "1234",
+    //     define: {
+    //         timestamps: true,
+    //         underscored: true,
+    //     },
+    // }
