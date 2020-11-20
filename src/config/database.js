1a762ca015c5393e24b79694d128ae17f4cfd5da
Junior Cunha <manoelricardo38@gmail.com>
2020-11-20 15:43:09 -0300

    Consertando arquivo do banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,27 @@
-// module.exports={
-//   dialect: "postgres",
-//   port: 5432,
-//   host: process.env.HOST ,
-//   schema: "public",
-//   database: process.env.DATABASE ,
-//   username:process.env.USER ,
-//   password: process.env.PASSWORD ,
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//   },
-// }
-
 module.exports = {
     dialect: "postgres",
     port: 5432,
-    host: "localhost",
+    host: process.env.HOST,
     schema: "public",
-    database: "smart",
-    username: "postgres",
-    password: "1234",
+    database: process.env.DATABASE,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
     define: {
         timestamps: true,
         underscored: true,
     },
 }
+
+// module.exports = {
+//     dialect: "postgres",
+//     port: 5432,
+//     host: "localhost",
+//     schema: "public",
+//     database: "smart",
+//     username: "postgres",
+//     password: "1234",
+//     define: {
+//         timestamps: true,
+//         underscored: true,
+//     },
+// }
